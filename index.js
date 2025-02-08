871b416ce8545a9664c2a6cf9cbd62f7cecc7ff1
dn4lele <daniema20@ac.sce.ac.il>
2025-02-08 23:14:19 +0200

    feat(index.html):add logo text

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const rooms = [226, 224, 115, 112];
+const rooms = [];
 
-document.addEventListener("DOMContentLoaded", () => {
+const loadRooms = () => {
   const roomList = document.getElementById("room-list");
   rooms.forEach((room) => {
     const option = document.createElement("option");
     option.appendChild(document.createTextNode(room));
     roomList.appendChild(option);
   });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  rooms.push("General"); // call the server to get the rooms
+
+  loadRooms();
 });
